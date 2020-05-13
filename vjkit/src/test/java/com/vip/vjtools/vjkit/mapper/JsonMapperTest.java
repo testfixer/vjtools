d4a039336602f6191f3ca537a43533ba7f6ed537
testfixer <testfixer0@gmail.com>
2020-05-13 12:34:17 -0500

    use JsonUnit to ignore the order of fields in expected JSON string

--- vjkit/src/test/java/com/vip/vjtools/vjkit/mapper/JsonMapperTest.java
+++ vjkit/src/test/java/com/vip/vjtools/vjkit/mapper/JsonMapperTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 
 /**
  * 测试Jackson对Object,Map,List,数组,枚举,日期类等的持久化. 更多测试见showcase中的JsonDemo.
@@ -27,7 +28,7 @@ public class JsonMapperTest {
 		TestBean bean = new TestBean("A");
 		String beanString = JsonMapper.INSTANCE.toJson(bean);
 		System.out.println("Bean:" + beanString);
-		assertThat(beanString)
+		assertThatJson(beanString)
 				.isEqualTo("{\"name\":\"A\",\"defaultValue\":\"hello\",\"nullValue\":null,\"emptyValue\":[]}");
 
 		// Map
@@ -36,7 +37,7 @@ public class JsonMapperTest {
 		map.put("age", 2);
 		String mapString = JsonMapper.INSTANCE.toJson(map);
 		System.out.println("Map:" + mapString);
-		assertThat(mapString).isEqualTo("{\"name\":\"A\",\"age\":2}");
+		assertThatJson(mapString).isEqualTo("{\"name\":\"A\",\"age\":2}");
 
 		// List<String>
 		List<String> stringList = Lists.newArrayList("A", "B", "C");
@@ -48,14 +49,14 @@ public class JsonMapperTest {
 		List<TestBean> beanList = Lists.newArrayList(new TestBean("A"), new TestBean("B"));
 		String beanListString = JsonMapper.INSTANCE.toJson(beanList);
 		System.out.println("Bean List:" + beanListString);
-		assertThat(beanListString).isEqualTo(
+		assertThatJson(beanListString).isEqualTo(
 				"[{\"name\":\"A\",\"defaultValue\":\"hello\",\"nullValue\":null,\"emptyValue\":[]},{\"name\":\"B\",\"defaultValue\":\"hello\",\"nullValue\":null,\"emptyValue\":[]}]");
 
 		// Bean[]
 		TestBean[] beanArray = new TestBean[] { new TestBean("A"), new TestBean("B") };
 		String beanArrayString = JsonMapper.INSTANCE.toJson(beanArray);
 		System.out.println("Array List:" + beanArrayString);
-		assertThat(beanArrayString).isEqualTo(
+		assertThatJson(beanArrayString).isEqualTo(
 				"[{\"name\":\"A\",\"defaultValue\":\"hello\",\"nullValue\":null,\"emptyValue\":[]},{\"name\":\"B\",\"defaultValue\":\"hello\",\"nullValue\":null,\"emptyValue\":[]}]");
 	}
 
@@ -149,17 +150,17 @@ public class JsonMapperTest {
 		// 打印全部属性
 		JsonMapper normalBinder = JsonMapper.defaultMapper();
 		TestBean bean = new TestBean("A");
-		assertThat(normalBinder.toJson(bean))
+		assertThatJson(normalBinder.toJson(bean))
 				.isEqualTo("{\"name\":\"A\",\"defaultValue\":\"hello\",\"nullValue\":null,\"emptyValue\":[]}");
 
 		// 不打印nullValue属性
 		JsonMapper nonNullMapper = JsonMapper.nonNullMapper();
-		assertThat(nonNullMapper.toJson(bean))
+		assertThatJson(nonNullMapper.toJson(bean))
 				.isEqualTo("{\"name\":\"A\",\"defaultValue\":\"hello\",\"emptyValue\":[]}");
 
 		// 不打印nullValue与empty的属性
 		JsonMapper nonEmptyMapper = JsonMapper.nonEmptyMapper();
-		assertThat(nonEmptyMapper.toJson(bean)).isEqualTo("{\"name\":\"A\",\"defaultValue\":\"hello\"}");
+		assertThatJson(nonEmptyMapper.toJson(bean)).isEqualTo("{\"name\":\"A\",\"defaultValue\":\"hello\"}");
 
 		TestBean nonEmptyBean = nonEmptyMapper.fromJson("{\"name\":\"A\",\"defaultValue\":\"hello\"}", TestBean.class);
 		assertThat(nonEmptyBean.getEmptyValue()).isEmpty();
